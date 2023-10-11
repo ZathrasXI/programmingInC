fb3111c3200b1649a8bc16db7d58b61636862ba4
Jan Phillips <janpatekphillips@gmail.com>
2023-10-11 15:05:55 +0100

    completed

--- ex3/music_playlisters.c
+++ ex3/music_playlisters.c
@@ -1,57 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <assert.h>
-
 
 #define MAX_LENGTH 20
 
-bool is_number_in_list(int number, int list[]);
-int get_number_not_in_list(int number, int list[], int upper_limit);
-
-
 
 int main(void){
-	assert(is_number_in_list(1, (int []) {3,4,5}) == false);
-	assert(is_number_in_list(1, (int []) {3,4,5,1}) == true);
-
 	int numbers_printed[MAX_LENGTH] = {0};
 	int songs_required, random_number = 0;
 	printf("How many songs required? ");
 	scanf("%d", &songs_required);
-	
+	int counter = 0;
 
-	for (int i = 0; i <= songs_required; i++){
-		random_number = rand() % 10;
-		bool number_already_printed = is_number_in_list(random_number, numbers_printed);
-		
-		if (!number_already_printed){
-			printf("%d\n", random_number);
-			numbers_printed[i] = random_number;
-		} else {
-			int another_random_number = get_number_not_in_list(random_number, numbers_printed, songs_required);
-			printf("%d\n", another_random_number);
+	while (counter < songs_required){
+		random_number = rand() % songs_required + 1;
+		if (numbers_printed[random_number] == 0){
+			printf("%d ", random_number);
+			numbers_printed[random_number] = 1;
+			counter++;
 		}
 	}
+	printf("\n");
+	return 0;
 }
 
 
-bool is_number_in_list(int number, int list[]) {
-	for (int i = 0; i < MAX_LENGTH; i++) {
-		if (number == list[i]){
-			return true;
-		}
-	}
-	return false;
-}
-
-
-int get_number_not_in_list(int number, int list[], int upper_limit){
-	int new_random_number = rand() % upper_limit;
-	while(is_number_in_list(new_random_number, list)){
-		new_random_number = rand() % upper_limit;
-	}
-	return new_random_number;
-}
 
 
