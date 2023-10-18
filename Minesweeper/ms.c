ad264a47608e9a25d862dddd6bdb4c7a2ab831ab
Jan Phillips <janpatekphillips@gmail.com>
2023-10-18 21:18:57 +0100

    remove functions no longer in use

--- Minesweeper/ms.c
+++ Minesweeper/ms.c
@@ -6,11 +6,9 @@
 #define ASCII_CONVERSION 48
 #define MAX_DIGIT 8 
 
-int unknown_cells_in_neighbourhood(int row, int column, int grid[MAXSQ][MAXSQ]);
 int adjacent_mines(int row, int column, int grid[MAXSQ][MAXSQ]);
 bool is_valid_number(char c);
 bool indexes_within_grid(int row, int column);
-bool should_be_a_mine(int row, int column, int grid[MAXSQ][MAXSQ]);
 int question_marks_in_neighbourhood(int row, int column, int grid[MAXSQ][MAXSQ]);
 int question_marks_to_mines(int row, int column, int grid[MAXSQ][MAXSQ], int changes);
 
@@ -129,7 +127,6 @@ board make_board(int totmines, int width, int height, char inp[MAXSQ*MAXSQ+1])
         }
         board_row++;
     }
-
     return new_board;
 }
 
@@ -154,22 +151,6 @@ bool is_valid_number(char c)
     return true;
 }
 
-int unknown_cells_in_neighbourhood(int row, int column, int grid[MAXSQ][MAXSQ])
-{
-    int cell_count = 0;
-    for (int i = row - 1; i <= row + 1; i++)
-    {
-        for (int j = column - 1; j <= column + 1; j++)
-        {
-            if (indexes_within_grid(i,j) && grid[i][j] == QUESTION_MARK && &grid[i][j] != &grid[row][column])
-            {
-                cell_count++;
-            }
-        }
-    }
-    return cell_count;
-}
-
 int adjacent_mines(int row, int column, int grid[MAXSQ][MAXSQ])
 {
     int mines = 0;
@@ -192,26 +173,6 @@ int adjacent_mines(int row, int column, int grid[MAXSQ][MAXSQ])
 bool indexes_within_grid(int row, int column)
 {
     return row >= 0 && row < MAXSQ && column >= 0 && column < MAXSQ;
-}
-
-bool should_be_a_mine(int row, int column, int grid[MAXSQ][MAXSQ])
-{
-    for (int r = row - 1; r <= row + 1; r++)
-    {
-        for (int c = column - 1; c <= column + 1; c++)
-        {
-            if (indexes_within_grid(r,c) && (r != row || c != column) && grid[r][c] < 9)
-            {
-                int mines = adjacent_mines(r,c,grid);
-                if (grid[r][c] > mines)
-                {
-                    printf("cell %d:%d value %d mines %d\n",r,c, grid[r][c], mines);
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
 }
 
 int question_marks_in_neighbourhood(int row, int column, int grid[MAXSQ][MAXSQ])
@@ -268,7 +229,6 @@ void test(void)
         {QUESTION_MARK,0,1,1,1}
     };
     int test_adjacent_mines[] = {0,0,0,0,0,  1,2,3,2,1,  1,1,3,2,2,  1,2,4,2,2, 0,0,1,1,1};
-    int test_unknown_cells[] = {0,1,0,1,0,  1,1,0,1,1, 0,0,0,0,0, 1,1,0,0,0, 0,1,0,0,0};
 
     // make_board()
     board test_board = make_board(5,5,5,"?110?123211XXX2124X2?0111");
@@ -294,16 +254,6 @@ void test(void)
         }
     }
     index = 0;
-    
-    // unknown_cells_in_neighbourhood()
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 5; j++)
-        {
-            assert(unknown_cells_in_neighbourhood(i,j,test_grid) == test_unknown_cells[index]);
-            index++;
-        }
-    }
 
     // indexes_within_grid()
     assert(indexes_within_grid(-1,0) == false);
@@ -311,32 +261,4 @@ void test(void)
     assert(indexes_within_grid(MAXSQ,MAXSQ) == false);
     assert(indexes_within_grid(0,0) == true);
     assert(indexes_within_grid(MAXSQ-1,MAXSQ-1) == true);
-
-    // should be a mine()
-    // int test_minefield[MAXSQ][MAXSQ] = {
-    //     {QUESTION_MARK,1,1,0,QUESTION_MARK},
-    //     {1,QUESTION_MARK,2,1,0},
-    //     {1,QUESTION_MARK,ASCII_X,QUESTION_MARK,1},
-    //     {0,1,2,QUESTION_MARK,1},
-    //     {QUESTION_MARK,0,1,1,1}
-    // };
-    // bool test_mines[] = {false,false,false,false,false,
-    //                     false,true,false,false,false,
-    //                     false,false,false,false,false,
-    //                     false,false,false,true,false,
-    //                     false,false,false,false,false};
-    
-    // int counter = 0;
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     for (int j = 0; j < 5; j++)
-    //     {
-    //         // assert(should_be_a_mine(i,j,test_minefield) == test_mines[counter]);
-    //         if (should_be_a_mine(i,j,test_minefield) != test_mines[counter])
-    //         {
-    //             printf("test minefinder: %d\n", counter);
-    //         }
-    //         counter++;
-    //     }
-    // }
 }
