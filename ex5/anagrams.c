a98a0f1b9a7f5d0f922acacaa537da4b86758956
Jan Phillips <janpatekphillips@gmail.com>
2023-10-25 10:15:55 +0100

    all anagrams are printed; tests for each function

--- ex5/anagrams.c
+++ ex5/anagrams.c
@@ -2,15 +2,24 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <assert.h>
 
 #define FILESIZE 25
 #define FILENAME "anagrams.txt"
 #define WORDSIZE 15
 
+//TODO should it warn me if I've imported a library and I'm not using anything from it? e.g. not using assert() from assert.h
+//TODO ask which is more efficient: function returns ul or I do more type casting?
+//TODO swap() using pointers
+//TODO don't print word if it is equal to input
+unsigned long len_without_newline_char(char str[WORDSIZE]);
+void test(void);
 bool is_anagram(char input[WORDSIZE], char word[WORDSIZE]);
+void sort(char str[WORDSIZE]);
 
 int main(int argc, char* argv[])
 {
+    test();
     if (argc != 2)
     {
         fprintf(stderr, "usage: ./anagrams <anagram string>\n");
@@ -24,16 +33,18 @@ int main(int argc, char* argv[])
     }
     char input[WORDSIZE];
     strcpy(input, argv[1]);
+    sort(input);
 
     FILE* anagram_file = fopen(FILENAME, "r");
     if (!anagram_file)
     {
         fprintf(stderr, "Unable to open %s\n", FILENAME);
     }
-    
+
     char buffer[WORDSIZE];
     while (fgets(buffer, WORDSIZE, anagram_file) != NULL)
     {
+        
         if (is_anagram(input, buffer))
         {
             printf("%s\n", buffer);
@@ -43,7 +54,7 @@ int main(int argc, char* argv[])
     int closed = fclose(anagram_file);
     if (closed != 0)
     {
-        fprintf(stderr,"issue closing %s\n", FILENAME);
+        fprintf(stderr,"issue closing file: %s\n", FILENAME);
         exit(EXIT_FAILURE);
     }
     return 0;
@@ -51,19 +62,63 @@ int main(int argc, char* argv[])
 
 bool is_anagram(char input[WORDSIZE], char word[WORDSIZE])
 {
-    if (strlen(input) != strlen(word) - 1)
+    if (strlen(input) != len_without_newline_char(word))
     {
-        printf("%lu - %s\n", strlen(word), word);
         return false;
+    }
+
+    char sorted_word[WORDSIZE];
+    strcpy(sorted_word, word);
+    sort(sorted_word);
+    for (int i = 0; i < (int) strlen(input); i++)
+    {
+        if (input[i] != sorted_word[i])
+        {
+            return false;
+        }
     }
     return true;
 }
 
-// void remove_new_line(char str)
-// {
-//     int length = (int) strlen(str);
-//     if (str[length-1] == '\n')
-//     {
-//         str[length-1] = '\0';
-//     }
-// }
+unsigned long len_without_newline_char(char str[WORDSIZE])
+{
+    int length = strlen(str);
+    if (str[length-1] == '\n')
+    {
+        str[length-1] = '\0';
+        return length - 1;
+    }
+    return length;
+}
+
+void sort(char str[WORDSIZE])
+{
+    char tmp;
+    int penultimate = (int) strlen(str) - 1;
+    for (int i = 0; i < penultimate; i++)
+    {
+        for (int j = i + 1; j < (int) strlen(str); j++)
+        {
+            if (str[i] > str[j])
+            {
+                tmp = str[i];
+                str[i] = str[j];
+                str[j] = tmp;
+            }
+        }
+    }
+}
+
+void test(void)
+{
+    char str[WORDSIZE] = "elephants\n";
+    assert(len_without_newline_char(str) == (unsigned long) 9);
+    strcpy(str, "elephants");
+    assert(len_without_newline_char(str) == (unsigned long) 9);
+
+    sort(str);
+    char sorted[WORDSIZE] = "aeehlnpst";
+    assert(strcmp(str, sorted) == 0);
+
+    assert(is_anagram(sorted, str) == true);
+}
