c530df9290876092d067dcc156320945c25ecbb4
Jan Phillips <janpatekphillips@gmail.com>
2023-11-09 17:23:37 +0000

    parse_args() returns true/false if valid/invalid input; more tests for parse_args()

--- n_queens/8q.c
+++ n_queens/8q.c
@@ -2,60 +2,80 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 #include <assert.h>
+#include <ctype.h>
 
 void test(void);
-bool parse_input(int *n, char* argv[], int argc, bool *verbose);
+bool parse_args(int *n, char* argv[], int argc, bool *verbose);
 
 int main(int argc, char* argv[])
 {
     test();
     int n;
     bool verbose = false;
-    bool acceptable_input = parse_input(&n, argv, argc, &verbose);
+    bool valid_input = parse_args(&n, argv, argc, &verbose);
 
+    if (!valid_input)
+    {
+        fprintf(stderr,"usage: ./8q <1-10> <optional: -verbose>\n");
+        exit(EXIT_FAILURE);
+    }
 
     if (verbose)
     {
-        printf("%d", verbose);
+        printf("n = %d verbose = %d\n", n, verbose);
     }
 
 }
 
-bool parse_input(int *n, char* argv[], int argc, bool *verbose)
+bool parse_args(int *n, char* argv[], int argc, bool *verbose)
 {
-    bool verbose = false;
-    for (int arg = 0; arg < argc; arg++)
+    if (argc < 2 || argc > 3)
     {
-        if (strcmp(argv[arg], "-verbose") == 0)
+        return false;
+    }
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (sscanf(argv[i], "%d", n) == 1)
         {
-            verbose = true;
+           if (*n < 1 || *n > 10)
+           {
+                return false;
+           } 
         }
-        if (sscanf(argv[arg], "%d", &n) == 1)
+        if (strcmp(argv[i], "-verbose") == 0)
         {
-            *n = argv[arg] - '0';
+            *verbose = true;
         }
     }
 
-    if (argc < 2 || argc > 3 || (argc == 3 && !verbose))
+    if (argc == 3 && *verbose == false)
     {
-        fprintf(stderr, "usage: ./8q <1-10> <optional: -verbose>\n");
-        exit(EXIT_FAILURE);
-        verbose = false;
+        return false;
     }
-    return verbose;
+    return true;
 }
 
 void test(void)
 {
-    int n;
-    char *test_argv[] = {"./8q", "-verbose", "3"};
-    assert(parse_input(&n, test_argv, 3) == true);
-    assert(n == 3);
+    int n_test;
+    bool verbose_test = false;
 
+    char *test_argv[] = {"./8q", "-verbose", "3"};
+    assert(parse_args(&n_test, test_argv, 3, &verbose_test) == true);
+    assert(n_test == 3);
+    assert(verbose_test = true);
+
+    verbose_test = false;
     char *test_argv1[] = {"./8q", "10", "-verbose"};
-    assert(parse_input(&n, test_argv1, 3) == true);
-    printf("n == %d\n", n);
-    assert(n == 10);
+    assert(parse_args(&n_test, test_argv1, 3, &verbose_test) == true);
+    assert(n_test == 10);
+    assert(verbose_test = true);
+
+    verbose_test = false;
+    char *test_argv2[] = {"./8q", "11"};
+    assert(parse_args(&n_test, test_argv2, 3, &verbose_test) == false);
+    assert(n_test == 11);
+    assert(verbose_test == false);
 }
