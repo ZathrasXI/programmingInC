079242bbe32eb5d153a2a071c7a1152a42248f09
Jan Phillips <janpatekphillips@gmail.com>
2023-11-29 16:00:24 +0000

    handling multiple bytes

--- ex9/packed_bool_array/pba.c
+++ ex9/packed_bool_array/pba.c
@@ -3,8 +3,9 @@
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
-
+#include <stdbool.h>
 #define INITIAL_SIZE 1
+#define BYTE 8
 
 typedef struct boolarr {
     int size;
@@ -16,13 +17,15 @@ typedef struct boolarr {
 boolarr *boolarr_init(void);
 boolarr *boolarr_initstr(const char* str);
 boolarr* boolarr_clone(const boolarr* ba);
+unsigned int boolarr_size(const boolarr* ba);
 void test(void);
 
 
 int main(void)
 {
-    test();
-    // boolarr *b = boolarr_init();
+    // test();
+    boolarr *b = boolarr_initstr("0000000900000001");
+    printf("\n%d\n", b->capacity);
     return 0;
 }
 
@@ -47,17 +50,43 @@ boolarr *boolarr_init(void)
 
 boolarr* boolarr_initstr(const char* str)
 {
-    boolarr *b = boolarr_init();
+    //for every 8 chars, calloc extra unint_8 to arr
     int len = strlen(str);
-    int bit = 0;
-    for (int i = len - 1; i >= 0; i--)
+    int bytes = len / BYTE;
+    if (len % BYTE != 0)
     {
-        if (str[i] == '1')
-        {
-            b->arr[0] |= (1 << bit);
-        }
-        bit++;
+        fprintf(stderr, "str needs to have length multiple of 8\n");
+        exit(EXIT_FAILURE);
     }
+    printf("bytes %d\n", bytes);
+
+    boolarr *b = (boolarr *) calloc(1, sizeof(boolarr));
+    b->arr = (uint8_t *) calloc(bytes, sizeof(uint8_t));
+    b->capacity = bytes;
+    b->size = bytes;
+    int bit = BYTE - 1;
+    int byte_n = 0;
+    
+    while (byte_n != bytes)
+    {
+        printf("%c", str[bit]);
+        bit--;
+        if (bit == (byte_n * BYTE - 1) - BYTE) 
+        {
+            byte_n++;
+            bit = bytes * BYTE - 1;
+        }
+    }
+    
+
+    // for (int i = len - 1; i >= 0; i--)
+    // {
+    //     if (str[i] == '1')
+    //     {
+    //         b->arr[0] |= (1 << bit);
+    //     }
+    //     bit++;
+    // }
     return b;
 }
 
@@ -67,9 +96,14 @@ boolarr* boolarr_clone(const boolarr* ba)
     deep_copy->capacity = ba->capacity;
     deep_copy->size = ba->size;
     memcpy(deep_copy->arr, ba->arr, sizeof(uint8_t) * ba->size);
-
     return deep_copy;
 }
+
+unsigned int boolarr_size(const boolarr* ba)
+{
+    return (unsigned int) ba->size;
+}
+
 
 
 void test(void)
@@ -79,6 +113,7 @@ void test(void)
     assert(b->capacity == 1);
     assert(b->size == 1);
 
+    // can init boolean packed array from string
     boolarr *s1 = boolarr_initstr("00000001");
     assert(s1->arr[0] == 1);
 
@@ -91,6 +126,10 @@ void test(void)
     boolarr *s129 = boolarr_initstr("10000001");
     assert(s129->arr[0] == 129);
 
+    boolarr *bytes_2 = boolarr_initstr("0000000000000001");
+    printf("%d\n",bytes_2->arr[0]);
+
+    // can deep copy
     boolarr *ba = boolarr_initstr("00000001");
     boolarr *deep_copy = boolarr_clone(ba);
     assert(deep_copy->size == ba->size);
