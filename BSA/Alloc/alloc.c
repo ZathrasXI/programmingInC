7a884570cfae97997b1e63bf175922737b9531f1
Jan Phillips <janpatekphillips@gmail.com>
2023-12-03 02:29:16 +0000

    can set specified index to value

--- BSA/Alloc/alloc.c
+++ BSA/Alloc/alloc.c
@@ -25,6 +25,13 @@ int _pow_2(int x)
     return 1 << x;
 }
 
+int _get_index_in_row(int index, int row)
+{
+    int zeroth_index_of_row = _pow_2(row) - 1;
+    int relative_index = index - zeroth_index_of_row;
+    return relative_index;
+}
+
 int bsa_maxindex(bsa *b)
 {
     //TODO return maxindex for when a row has been written to
@@ -46,7 +53,7 @@ int bsa_maxindex(bsa *b)
     }
 }
 
-int _get_row_index(int cell_index)
+int _get_row(int cell_index)
 {
     int row = 0;
     cell_index++;
@@ -57,9 +64,31 @@ int _get_row_index(int cell_index)
     return row;
 }
 
+bool bsa_set(bsa *b, int indx, int d)
+{
+    //TODO check that I have understood when to return false
+    int row = _get_row(indx);
+    if (b->rows[row]->data == NULL)
+    {
+        b->rows[row]->data = (int *) calloc(b->rows[row]->length, sizeof(int));
+        if (b->rows[row]->data == NULL)
+        {
+            fprintf(stderr, "error allocating memory for unset row #%d\n", row);
+            return false;
+        }
+    }
+
+    int i = _get_index_in_row(indx, row);
+    *(b->rows[row]->data + i) = d;
+
+    return true;
+}
+
 void test(void)
 {   
-    //bitwise i ** 2
+    /*
+    bitwise i ** 2
+    */
     assert(_pow_2(0) == 1);
     assert(_pow_2(1) == 2);
     assert(_pow_2(2) == 4);
@@ -78,24 +107,66 @@ void test(void)
         assert(test_bsa->rows[i]->length == _pow_2(i));
     }
 
-    // bsa_maxindex() returns -1 when given a NULL BSA
+    /*
+    bsa_maxindex() returns -1 when given a NULL BSA
+    */
     assert(test_bsa->max_index == -1);
     assert(bsa_maxindex(test_bsa) == -1);
 
-    // free after test
+    // free bsa *test_bsa after test
     for (int i = 0; i < BSA_ROWS; i++)
     {
         free(test_bsa->rows[i]->data);
     }
     free(test_bsa);
 
-    // can get index of row based on index given
-    assert(_get_row_index(0) == 0);
-    assert(_get_row_index(1) == 1);
-    assert(_get_row_index(2) == 1);
-    assert(_get_row_index(29) == 4);
-    assert(_get_row_index(536870912) == 29);
+    /*
+    can get index of row based on index given
+    */ 
+    assert(_get_row(0) == 0);
+    assert(_get_row(1) == 1);
+    assert(_get_row(2) == 1);
+    assert(_get_row(29) == 4);
+    assert(_get_row(536870912) == 29);
 
+    /*
+    get index for data within the row 
+    based upon the requested index
+    */
+    int row = _get_row(0);
+    assert(_get_index_in_row(0, row) == 0);
+
+    row = _get_row(536870911);
+    assert(_get_index_in_row(536870911, row) == 0);
+
+    row = _get_row(30);
+    assert(_get_index_in_row(30, row) == 15);
+
+    /*
+    can set value of requested index
+    */
+    bsa *test_set = bsa_init();
+    assert(bsa_set(test_set,0,0));
+    assert(*test_set->rows[0]->data == 0);
+
+    assert(bsa_set(test_set,2,1));
+    assert(*(test_set->rows[1]->data + 1) == 1);
+
+    assert(bsa_set(test_set, 6, 36));
+    assert(*(test_set->rows[2]->data + 3) == 36);
+
+    assert(bsa_set(test_set,536870911,1));
+    assert(*(test_set->rows[29]->data + 0) == 1);
+
+    assert(bsa_set(test_set,536870911 * 2, 99));
+    assert(*(test_set->rows[29]->data + 536870911) == 99);
+
+    // free bsa *test_set after test
+    for (int i = 0; i < BSA_ROWS; i++)
+    {
+        free(test_set->rows[i]->data);
+    }
+    free(test_set);
 }
 
 
--- BSA/Alloc/specific.h
+++ BSA/Alloc/specific.h
@@ -5,6 +5,7 @@
 
 int _pow_2(int x);
 int _get_row_index(int cell_index);
+int _get_index_in_row(int i, int row);
 
 typedef struct row
 {
