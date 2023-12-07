d2ba2c1f07b1ec5581197619ab5527fc90bef662
Jan Phillips <janpatekphillips@gmail.com>
2023-12-07 20:23:35 +0000

    can set left child

--- BSA/Extension/extension.c
+++ BSA/Extension/extension.c
@@ -31,12 +31,45 @@ bool bsa_set(bsa *b, int indx, int d)
             exit(EXIT_FAILURE);
         }
         b->root->index = indx;
-        b->root->value = d;
+        b->root->data = d;
         b->root->height = 0;
         b->root->left = NULL;
         b->root->right = NULL;
-    }
+        b->max_index = indx;
     return true;
+    }
+
+    if (indx >= b->root->index)
+    {
+        b->root->right = (Node *) calloc(INIT_SIZE, sizeof(Node));
+        if (b->root->right == NULL)
+        {
+            fprintf(stderr, "failed to allocate memory for node\n");
+            exit(EXIT_FAILURE);
+        }
+        b->root->right->index = indx;
+        b->root->right->data = d;
+        b->root->right->left = NULL;
+        b->root->right->right = NULL;
+        b->max_index = indx;
+        return true;
+    }
+    else if (indx < b->root->index)
+    {
+        b->root->left = (Node *) calloc(INIT_SIZE, sizeof(Node));
+        if (b->root->left == NULL)
+        {
+            fprintf(stderr, "failed to allocate memory for node\n");
+            exit(EXIT_FAILURE);
+        }
+        b->root->left->index = indx;
+        b->root->left->data = d;
+        b->root->left->left = NULL;
+        b->root->left->right = NULL;
+        b->max_index = indx;
+        return true;
+    }
+    return false;
 }
 
 void test()
@@ -53,18 +86,25 @@ void test()
 
     /*
     bsa_set()
-    assign value to index
+    assign data to index
     */
-   bsa *test_set = bsa_init();
-   //set root
-   assert(bsa_set(test_set, 10,10));
-   assert(test_set->root->index == 10);
-   assert(test_set->root->value == 10);
-   assert(test_set->root->height == 0);
-   assert(test_set->root->left == NULL);
-   assert(test_set->root->right == NULL);
-   free(test_set->root);
-   free(test_set);
+    bsa *test_set = bsa_init();
+    //set root
+    assert(bsa_set(test_set, 10,10));
+    assert(test_set->root->index == 10);
+    assert(test_set->root->data == 10);
+    assert(test_set->root->height == 0);
+    assert(test_set->root->left == NULL);
+    assert(test_set->root->right == NULL);
+    assert(test_set->max_index == 10);
+
+    //can set left child
+    assert(bsa_set(test_set,9, 9));
+
+    //clean up
+    free(test_set->root->left);
+    free(test_set->root);
+    free(test_set);
 
 
 
