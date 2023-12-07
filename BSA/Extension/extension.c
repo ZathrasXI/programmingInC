d1c4b1080f4aefea8b0d8f587bb39552bdde596f
Jan Phillips <janpatekphillips@gmail.com>
2023-12-07 20:00:25 +0000

    can init root

--- BSA/Extension/extension.c
+++ BSA/Extension/extension.c
@@ -19,12 +19,54 @@ bsa *bsa_init(void)
     return b;
 }
 
+bool bsa_set(bsa *b, int indx, int d)
+{
+    //tree is empty
+    if (b->root == NULL)
+    {
+        b->root = (Node *) calloc(INIT_SIZE, sizeof(Node));
+        if (b->root == NULL)
+        {
+            fprintf(stderr, "index not set: error allocating memory\n");
+            exit(EXIT_FAILURE);
+        }
+        b->root->index = indx;
+        b->root->value = d;
+        b->root->height = 0;
+        b->root->left = NULL;
+        b->root->right = NULL;
+    }
+    return true;
+}
+
 void test()
 {
+    /*
+    bsa_init() 
+    create empty BSA
+    */
     bsa *test_init = bsa_init();
     assert(test_init->max_index == NOT_SET);
     assert(test_init->root == NULL);
     //clean up
     free(test_init);
+
+    /*
+    bsa_set()
+    assign value to index
+    */
+   bsa *test_set = bsa_init();
+   //set root
+   assert(bsa_set(test_set, 10,10));
+   assert(test_set->root->index == 10);
+   assert(test_set->root->value == 10);
+   assert(test_set->root->height == 0);
+   assert(test_set->root->left == NULL);
+   assert(test_set->root->right == NULL);
+   free(test_set->root);
+   free(test_set);
+
+
+
 
 }
