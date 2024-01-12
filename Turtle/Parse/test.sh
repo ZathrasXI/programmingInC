12cfc0909006ba62dee97582fea36ec1c24bd5f6
Jan Phillips <janpatekphillips@gmail.com>
2024-01-12 11:46:47 +0000

    compiles using gcc and clang with sanitizer and runs against all files in ../TTLs/

--- Turtle/Parse/test.sh
+++ Turtle/Parse/test.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-# Check if the Makefile exists
-if [ ! -e "../Parse/Makefile" ]; then
-    echo "No Makefile found"
-    exit 1
-fi
+#compile parser using gcc and clang,
+#run both files against all of the test files in ../TTL/
+#assert that the exit status is correct
 
-# Compile parser
-cd ../Parse
-make test >> ../Testing/results.parser
-if [ $? -ne 0 ]; then 
-    echo "Error compiling"
-    exit 1
-fi
+#define flags
+COMMON="-Wall -Wextra -Wfloat-equal -Wpedantic -Wvla -std=c99 -Werror"
+DEBUG="-g3"
+SANITIZE="$COMMON -fsanitize=undefined -fsanitize=address $DEBUG"
 
-# Run parser against every file
-TTLs="../TTLs"
-expected_failure="fail_parse_ok_interp.ttl"
+compilers=("gcc" "clang")
+for comp in "${compilers[@]}"; do
 
-for file in "$TTLs"/*; do
-    if [ -f "$file" ]; then
-        ./test_parser "$file" >> ../Testing/results.parser
-        if [[ "$file" == "$expected_failure" && $? == 0 ]]; then
-            echo "test $file failed" >> ../Testing/results.parser
-        fi
-        if [[ "$file" != "$expected_failure" && $? != 0 ]]; then
-            echo "test $file failed" >> ../Testing/results.parser
-        fi
+    # Compile parser using gcc
+    ${comp} parse.c ${SANITIZE} -o test_${comp}_parser
+    if [ $? -ne 0 ]; then 
+        echo "Error compiling"
+        exit 1
     fi
+
+    # Run parser against every file
+    TTLs="../TTLs"
+    expected_failure="fail_parse_ok_interp.ttl"
+
+    for file in "$TTLs"/*; do
+        if [ -f "$file" ]; then
+            ./test_${comp}_parser "$file" 2> /dev/null
+            if [[ "$file" == "$expected_failure" && $? == 0 ]]; then
+                echo "FAIL: compiler: ${comp} - $file" 
+            fi
+            if [[ "$file" != "$expected_failure" && $? != 0 ]]; then
+                echo "FAIL: compiler: ${comp} - $file" 
+            fi
+        fi
+    done
+
+    #rm compiled file 
+    rm test_${comp}_parser
 done
