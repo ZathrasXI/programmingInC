f9f69579b5ef30d2ab01db4d0744c997e25a3ed1
Jan Phillips <janpatekphillips@gmail.com>
2024-01-24 13:20:01 +0000

    test that output file is correct

---
+++ Turtle/Interp/test_argv2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+files=("octagon1" "octagon2" "spiral" "tunnel" "turn" "donothing" "downarrow" "empty" "labyrinth")
+
+mkdir testing
+make
+
+for file in "${files[@]}"; do
+    ./interpreter "../TTLs/$file.ttl" "./testing/$file.out"
+    cmp "./test_files/$file.out" "./testing/$file.out"
+    if [[ $? != 0 ]]; then 
+        echo "FAIL: files are not identical - "../TTLs/$file.ttl" and "./testing/$file.out""
+        rm -r testing/
+        exit 1
+    fi
+done
+
+rm -r testing/
